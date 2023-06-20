64e0d00323eddb53e7d32e7a55d3f2cdefd85a34
retnodamayanti <mayahere5@gmail.com>
2023-06-20 16:37:38 +0930

    edited typo

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,7 +62,6 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    // Delete one tag by its `id` value
     const tag = await Tag.destroy({
       where: { id: req.params.id },
     });
